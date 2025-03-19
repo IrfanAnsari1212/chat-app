f252ce5e3117c57ac81b960e4b0a7b476e70c1a0
Irfan Ansari <151234719+IrfanAnsari1212@users.noreply.github.com>
2025-03-19 14:31:13 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5003/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://chat-app-ec4f.onrender.com" : "/api",
   withCredentials: true,
 });
