f6015b3b179662299c3d734d5bb5571721b6a723
Irfan Ansari <151234719+IrfanAnsari1212@users.noreply.github.com>
2025-03-19 14:44:04 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://chat-app-frontend-7crf.onrender.com",
     credentials: true,
 }));
 
